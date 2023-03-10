deae3c99ea13090050c32384aaff85296e102bfa
chrislies <clai20707@gmail.com>
2023-03-09 22:46:32 -0500

    implemented game window for each difficulty

--- main.js
+++ main.js
@@ -1,20 +1,56 @@
+const gw = document.getElementById("game-window");
+
+function generateEasy() {
+  // console.log(gw.rows.length);
+  if (gw.rows.length > 0) {
+    while (gw.rows.length > 0) {
+      gw.deleteRow(0);
+    }
+  } 
+  let n = 7; // n x n grid
+  for (let i=0; i<n; i++) {
+    let row = document.createElement("tr");
+    for (let j=0; j<n; j++) {
+      let data = document.createElement("td");
+      row.appendChild(data);
+    }
+    gw.appendChild(row);
+  }
+}
+
 function generateMedium() {
-  let gw = document.getElementById("game-window");
-  console.log(gw.rows.length);
-  if (gw.rows.length>0) {
-    for (let i = gw.rows.legnth; i > 0; i--) {
-      gw.rows[i].remove();
+  // console.log(gw.rows.length);
+  if (gw.rows.length > 0) {
+    while (gw.rows.length > 0) {
+      gw.deleteRow(0);
     }
-  } else { 
-    let n = 7; // n x n grid
-    for (let i=0; i<n; i++) {
-      let row = document.createElement("tr");
-      for (let j=0; j<n; j++) {
-        let data = document.createElement("td");
-        row.appendChild(data);
-      }
-      gw.appendChild(row);
+  } 
+  let n = 10; // n x n grid
+  for (let i=0; i<n; i++) {
+    let row = document.createElement("tr");
+    for (let j=0; j<n; j++) {
+      let data = document.createElement("td");
+      row.appendChild(data);
     }
+    gw.appendChild(row);
+  }
+}
+
+function generateHard() {
+  // console.log(gw.rows.length);
+  if (gw.rows.length > 0) {
+    while (gw.rows.length > 0) {
+      gw.deleteRow(0);
+    }
+  } 
+  let n = 13; // n x n grid
+  for (let i=0; i<n; i++) {
+    let row = document.createElement("tr");
+    for (let j=0; j<n; j++) {
+      let data = document.createElement("td");
+      row.appendChild(data);
+    }
+    gw.appendChild(row);
   }
 }
 
@@ -23,15 +59,16 @@ selectDifficulty.addEventListener("change", () => {
   const selectedValue = selectDifficulty.value;
   switch(selectedValue) {
     case "easy":
-      console.log("Easy");
-      // generateEasy();
+      // console.log("Easy");
+      generateEasy();
       break;
     case "medium":
-      console.log("Medium");
+      // console.log("Medium");
       generateMedium();
       break
     case "hard":
-      console.log("Hard");
+      // console.log("Hard");
+      generateHard();
       break;
     default:
       console.log("Select difficulty!")
